387e6a3aedb4d3b24e4121d26dcf15f340283ca2
John Knotts <jhnnyk@gmail.com>
2017-06-03 16:07:03 -0600

    binding values lesson complete

--- Build a Simple Dynamic Site with Node.JS/my_code/renderer.js
+++ Build a Simple Dynamic Site with Node.JS/my_code/renderer.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
+function mergeValues(values, content) {
+  // Cycle over the keys
+  for (let key in values) {
+    // Replace all {{key}} with the value from the values object
+    content = content.replace(`{{${key}}}`, values[key]);
+    console.log(values);
+  }
+  // return merged content
+  return content;
+}
+
 function view(templateName, values, response) {
   // Read from the template file
-  let fileContents = fs.readFileSync('./views/' + templateName + '.html');
+  let fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
   // Insert values into the content
-
+  fileContents = mergeValues(values.values, fileContents);
   // Write out the contents to the response
   response.write(fileContents);
 }

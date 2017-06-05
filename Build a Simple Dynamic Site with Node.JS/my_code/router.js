5600529817ab0defaa267cb4f4b72bd51584cb80
John Knotts <jhnnyk@gmail.com>
2017-06-05 07:01:02 -0600

    redirection headers in Node.js

--- Build a Simple Dynamic Site with Node.JS/my_code/router.js
+++ Build a Simple Dynamic Site with Node.JS/my_code/router.js
@@ -21,9 +21,9 @@ function home(request, response) {
       request.on("data", function(postBody) {
         // extract the username
         let query = querystring.parse(postBody.toString());
-        response.write(query.username);
-        response.end();
         // redirect to /:username
+        response.writeHead(303, {"Location": "/" + query.username});
+        response.end();
       });
 
     }
